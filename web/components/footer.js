3294e7145dd02ab2659d832143e8ed09389b02c2
Ådne Forslund <adne.forslund@gmail.com>
2022-05-31 15:57:46 +0200

    update with components

---
+++ web/components/footer.js
@@ -0,0 +1,14 @@
+const Footer = () => (
+  <footer>
+    <div className="footer-inside">
+      <div className="footer-text">
+        <p>Live long and prosper</p>
+      </div>
+    </div>
+    <div className="footer-panel">
+      <span className="hop">22</span>47
+    </div>
+  </footer>
+);
+
+export default Footer;
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
+
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
@@ -10,147 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="wrap">
-        <div className="left-frame-top">
-          <div className="panel-1">
-            <a href=""></a>
-          </div>
-          <div className="panel-2">
-            02<span className="hop">-262000</span>
-          </div>
-        </div>
-        <div className="right-frame-top">
-          <div className="banner">LCARS &#149; Online</div>
-          <div className="data-cascade-button-group">
-            <div className="cascade-wrapper">
-              <div className="data-cascade">
-                <div className="row-1">
-                  <div className="dc1">101</div>
-                  <div className="dc2">7109</div>
-                  <div className="dc3">1966</div>
-                  <div className="dc4">1222</div>
-                  <div className="dc5">2020</div>
-                  <div className="dc6">1444</div>
-                  <div className="dc7">102</div>
-                  <div className="dc8">1103</div>
-                  <div className="dc9">1935</div>
-                  <div className="dc10">1940</div>
-                  <div className="dc11">708</div>
-                  <div className="dc12">M113</div>
-                  <div className="dc13">1956</div>
-                  <div className="dc14">1209</div>
-                </div>
-                <div className="row-2">
-                  <div className="dc1">102</div>
-                  <div className="dc2">8102</div>
-                  <div className="dc3">1987</div>
-                  <div className="dc4">044</div>
-                  <div className="dc5">0051</div>
-                  <div className="dc6">607</div>
-                  <div className="dc7">1976</div>
-                  <div className="dc8">1031</div>
-                  <div className="dc9">1984</div>
-                  <div className="dc10">1954</div>
-                  <div className="dc11">1103</div>
-                  <div className="dc12">415</div>
-                  <div className="dc13">1045</div>
-                  <div className="dc14">1864</div>
-                </div>
-                <div className="row-3">
-                  <div className="dc1">103</div>
-                  <div className="dc2">714</div>
-                  <div className="dc3">1993</div>
-                  <div className="dc4">0222</div>
-                  <div className="dc5">052</div>
-                  <div className="dc6">1968</div>
-                  <div className="dc7">2450</div>
-                  <div className="dc8">746</div>
-                  <div className="dc9">56</div>
-                  <div className="dc10">47</div>
-                  <div className="dc11">716</div>
-                  <div className="dc12">8719</div>
-                  <div className="dc13">417</div>
-                  <div className="dc14">602</div>
-                </div>
-                <div className="row-4">
-                  <div className="dc1">104</div>
-                  <div className="dc2">6104</div>
-                  <div className="dc3">1995</div>
-                  <div className="dc4">322</div>
-                  <div className="dc5">90</div>
-                  <div className="dc6">1931</div>
-                  <div className="dc7">1701</div>
-                  <div className="dc8">51</div>
-                  <div className="dc9">29</div>
-                  <div className="dc10">218</div>
-                  <div className="dc11">908</div>
-                  <div className="dc12">2114</div>
-                  <div className="dc13">85</div>
-                  <div className="dc14">3504</div>
-                </div>
-                <div className="row-5">
-                  <div className="dc1">105</div>
-                  <div className="dc2">08</div>
-                  <div className="dc3">2001</div>
-                  <div className="dc4">713</div>
-                  <div className="dc5">079</div>
-                  <div className="dc6">1940</div>
-                  <div className="dc7">LV</div>
-                  <div className="dc8">426</div>
-                  <div className="dc9">105</div>
-                  <div className="dc10">10</div>
-                  <div className="dc11">1206</div>
-                  <div className="dc12">1979</div>
-                  <div className="dc13">402</div>
-                  <div className="dc14">795</div>
-                </div>
-                <div className="row-6">
-                  <div className="dc1">106</div>
-                  <div className="dc2">31</div>
-                  <div className="dc3">2017</div>
-                  <div className="dc4">429</div>
-                  <div className="dc5">65</div>
-                  <div className="dc6">871</div>
-                  <div className="dc7">1031</div>
-                  <div className="dc8">541</div>
-                  <div className="dc9">656</div>
-                  <div className="dc10">764</div>
-                  <div className="dc11">88</div>
-                  <div className="dc12">001</div>
-                  <div className="dc13">27</div>
-                  <div className="dc14">05</div>
-                </div>
-              </div>
-            </div>
-            <div className="button-col">
-              <div className="button" id="top-left">
-                <a href="">01</a>
-              </div>
-              <div className="button" id="bottom-left">
-                <a href="">02</a>
-              </div>
-            </div>
-            <div className="button-col">
-              <div className="button" id="top-right">
-                <a href="">03</a>
-              </div>
-              <div className="button" id="bottom-right">
-                <a href="">04</a>
-              </div>
-            </div>
-          </div>
-          <div className="top-corner-bg">
-            <div className="top-corner"></div>
-          </div>
-          <div className="bar-panel">
-            <div className="bar-1"></div>
-            <div className="bar-2"></div>
-            <div className="bar-3"></div>
-            <div className="bar-4"></div>
-            <div className="bar-5"></div>
-          </div>
-        </div>
-      </div>
+      <Header />
       <div className="wrap" id="gap">
         <div className="left-frame">
           <div>
@@ -198,18 +60,32 @@ export default function Home() {
 
             <h2>Welcome to LCARS &#149; classNameic Theme</h2>
 
-            <p>Live long and prosper.</p>
-
-            <footer>
-              <div className="footer-inside">
-                <div className="footer-text">
-                  <p>Live long and prosper</p>
-                </div>
-              </div>
-              <div className="footer-panel">
-                <span className="hop">22</span>47
-              </div>
-            </footer>
+            <p>
+              Some methods for creating work, such as employing intuition, are
+              shared across the disciplines within the applied arts and fine
+              art. Mark Getlein, the writer, suggests the principles of design
+              are "almost instinctive", "built-in", "natural", and part of "our
+              sense of 'rightness'." However, the intended application and
+              context of the resulting works will vary greatly. This is not to
+              say that production never involves problem-solving or creativity,
+              nor that design always involves creativity. Designs are rarely
+              perfect and are sometimes repetitive. The imperfection of a design
+              may task a production position (e.g. production artist,
+              construction worker) with utilizing creativity or problem-solving
+              skills to compensate for what was overlooked in the design
+              process. Likewise, a design may be a simple repetition (copy) of a
+              known preexisting solution, requiring minimal, if any, creativity
+              or problem-solving skills from the designer. "Process design" (in
+              contrast to the "design process" mentioned above) is the planning
+              of routine steps of a process aside from the expected result.
+              Processes (in general) are treated as a product of design, not the
+              method of design. The term originated with the industrial
+              designing of chemical processes. With the increasing complexities
+              of the information age, consultants and executives have found the
+              term useful to describe the design of business processes as well
+              as manufacturing processes.
+            </p>
+            <Footer />
           </div>
         </div>
       </div>
